4920c0ab11fd9b3bc3f69672a07787fa5f8853d9
abhinav <abhinav.i@knowlarity.com>
2016-09-09 22:38:29 +0530

    Added dynamic rendering

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,6 @@
 var options = {tooltipTemplate:
 	" <%=label%>: <%= numeral(value).format('($00[.]00)') %> - <%= numeral(circumference / 6.283).format('(0[.][00]%)') %>"
    }
-
 var data = {
     labels: [
         "Ninja",
@@ -41,3 +40,44 @@ var myDoughnutChart = new Chart(ctx, {
 
 var leading_team = $("tr td:nth-child(2)").first().text()
 $(".leading-team p").text(leading_team)
+
+
+/* 
+ Event source stuff
+*/
+eventSource = null
+
+function connectToStream() {
+    eventSource = new EventSource('http://52.50.164.222:8001/leaderboard-stream');
+    eventSource.addEventListener('message', listener);
+}
+
+function listener(event) {
+    data = JSON.parse(event.data)
+    console.log(data);
+
+    // If there are leaders.
+    if (data.leaderboard.length) {
+        $('#results').html('');
+        $.each(data.leaderboard, function(index, row) {
+            $('#results').append(`
+                <tr>
+                    <td>${index + 1}.</td>
+                    <td>${row.team}</td>
+                    <td>${row.hotline_number}</td>
+                    <td>${row.votes}</td>
+                </tr>
+                `);
+        });  
+
+        total_vote_percentage = (data.participation_count / data.participant_count) * 100;
+        $('#vote_total').text(`${data.participation_count}`);  
+        $('#vote_engagement').text(`${total_vote_percentage}%`)
+    }
+    
+    
+}
+
+connectToStream();
+eventSource.addEventListener('message', listener);
+eventSource.addEventListener('error', connectToStream);
